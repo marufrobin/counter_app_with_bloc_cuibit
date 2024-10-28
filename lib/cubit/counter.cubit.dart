4ae927edc54690a25bfbf8d2216c553b6efd66bb
Maruf Robin <marufrobin5@gmail.com>
2024-10-28 17:50:09 +0600

    ✅ Counter using cubit and BlocBuilder done

--- lib/cubit/counter.cubit.dart
+++ lib/cubit/counter.cubit.dart
@@ -1,5 +1,19 @@
+import 'dart:developer';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<int> {
   CounterCubit() : super(5);
+
+  void increment() {
+    log(name: "Before: ", state.toString());
+    emit(state + 1);
+    log(name: "After: ", state.toString());
+  }
+
+  void decrement() {
+    log(name: "Before: ", state.toString());
+    emit(state > 0 ? state - 1 : state);
+    log(name: "After: ", state.toString());
+  }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,44 +1,60 @@
+import 'dart:developer';
+
 import 'package:counter_app_with_bloc_cuibit/cubit/counter.cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final counter = CounterCubit().state;
+  final counterCubit = CounterCubit();
 
   @override
   Widget build(BuildContext context) {
+    final counter = counterCubit.state;
+    log(name: "--> ", "Checking if full build");
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$counter',
+      appBar: _appBar(context),
+      body: BlocBuilder<CounterCubit, int>(
+        bloc: counterCubit,
+        builder: (context, state) {
+          return Center(
+            child: Text(
+              'Count: $state',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ],
-        ),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () => counterCubit.increment(),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 20),
+          FloatingActionButton(
+            onPressed: () => counterCubit.decrement(),
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
       ),
+    );
+  }
+
+  AppBar _appBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: const Text("Cubit Counter Demo"),
     );
   }
 }
